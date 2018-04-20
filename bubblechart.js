520d047430cd382dd5a3079c684ddb0c6bb68289
CosminP\Cosmin <cosmin.andrei.popa@gmail.com>
2018-04-21 02:33:41 +0300

    1 fix si trec la animatie

--- bubblechart.js
+++ bubblechart.js
@@ -1,7 +1,7 @@
 var canvasChart = function(){
 
     var ctx,
-    margin = {top: 40, left: 75, right: 0, bottom: 75},
+    margin = {top: 40, left: 75, right:0, bottom: 75},
     maxYValue = 0, 
     maxXValue = 0,
     ratio = 0,
@@ -21,15 +21,19 @@ var canvasChart = function(){
          chartWidth = canvas.getAttribute('width');
          canvas.addEventListener('mousemove', mouseMove, false);
          ctx = canvas.getContext("2d");
-
-          xMax = chartWidth - (margin.left + margin.right);
+          xMax = chartWidth -  (margin.left + margin.right);
           yMax = chartHeight - (margin.top + margin.bottom);
-         maxYValue = 1;
+         maxYValue = Math.ceil(getMaxYValue());
          maxXValue = getMaxXValue();
          maxPop = getMaxPopulation();
-         ratio = yMax / maxYValue / 5;
-         xRatio = xMax / maxXValue;
+         maxRadius = getPointRadius(maxPop);
 
+         ratio = yMax / maxYValue;
+         xRatio = xMax / maxXValue;
+        debugger
+         margin.right += pointRadius;
+         margin.bottom += pointRadius;
+         
          if( data.renderTypes === undefined || data.renderTypes == null)
             data.renderTypes = [renderType.lines];
         renderParts();
@@ -45,13 +49,13 @@ var canvasChart = function(){
     },
 
     renderBackround = function(){
-        var lingrad = ctx.createLinearGradient(margin.left, margin.top, xMax - margin.right, yMax);
+        var lingrad = ctx.createLinearGradient(margin.left, margin.top, xMax - margin.right, yMax - margin.bottom);
         lingrad.addColorStop(0.0, '#D4D4D4');
         lingrad.addColorStop(0.2, '#fff');
         lingrad.addColorStop(0.8, '#fff');
         lingrad.addColorStop(1, '#D4D4D4');
         ctx.fillStyle = lingrad;
-        ctx.fillRect(margin.left, margin.top, xMax - margin.left, yMax - margin.top);
+        ctx.fillRect(margin.left, margin.top, xMax , yMax );
         ctx.fillStyle = 'black'
     },
 
@@ -63,43 +67,44 @@ var canvasChart = function(){
         ctx.fillText(data.title, (chartWidth / 2), margin.top/2);
 
         var txtSize = ctx.measureText(data.xLabel);
-        ctx.fillText(data.xLabel, margin.right + (xMax / 2) - (txtSize.width / 2), yMax + (margin.bottom / 1.2));
+        ctx.fillText(data.xLabel, margin.right + (xMax / 2) - (txtSize.width / 2), yMax + margin.top + (margin.bottom / 1.2));
 
         ctx.save();
         ctx.rotate(-Math.PI / 2);
         ctx.font = labelFont;
-        ctx.fillText(data.yLabel, (yMax / 2) * -1, margin.left / 4);
+        ctx.fillText(data.yLabel, ((yMax + margin.top) / 2) * -1, margin.left / 4);
         ctx.restore();
     },
 
     renderLinesAndLabels = function(shouldRenderText){
-        var yInc = yMax / data.dataPoints.length;
+        var yInc = (yMax + margin.top) / 20;
         var yPos = 0;
-        var xInc = getXInc();
-        var xPos = margin.left;
+        var xInc = (xMax + margin.left)  / 20 ;
+        var xPos = 0;
+        var ceiledYMaxValue = Math.ceil(maxYValue)
+        for (var i = 0; i < 20; i++){
+            yPos +=  yInc;
+            xPos += (i ==0 ) ? margin.left: xInc;
 
-        for (var i = 0; i < data.dataPoints.length; i++){
-            yPos += (i ==0 ) ? margin.top : yInc;
-            
-            drawLine({ x : margin.left, y: yPos, x2: xMax, y2: yPos}, '#E8E8E8');
-
+            drawLine({ x : margin.left, y: yPos, x2: xMax + margin.left, y2: yPos}, '#E8E8E8');
             if(shouldRenderText){
                 ctx.font = (data.dataPointFont != null) ? data.dataPointFont : '10pt Calibri';
-                var txt = Math.round(maxYValue - ((i == 0 ) ? 0 :yPos / ratio));
+                var txt =  Math.ceil( (ceiledYMaxValue - (ceiledYMaxValue / 20) * (i+1))*100) / 100;
+                debugger
                 var txtSize = ctx.measureText(txt);
                 ctx.fillText(txt, margin.left - ((txtSize.width >= 14) ? txtSize.width: 10) -7 , yPos + 4);
                 
 
-                txt = data.dataPoints[i].x;
+                txt = Math.ceil((maxXValue / 20) *100 * i) / 100;
                 txtSize = ctx.measureText(txt);
-                ctx.fillText(txt, xPos, yMax + (margin.bottom / 3));
-                xPos += xInc;
+                ctx.fillText(txt, xPos, yMax + margin.top + (margin.bottom / 3));
+               
             }
         }
 
-        drawLine({ x: margin.left, y: margin.top, x2: margin.left, y2: yMax});
+        drawLine({ x: margin.left, y: margin.top, x2: margin.left, y2: yMax + margin.top});
 
-        drawLine({x: margin.left, y: yMax, x2: xMax, y2: yMax});
+        drawLine({x: margin.left, y: yMax + margin.top, x2: xMax + margin.left, y2: yMax + margin.top});
     },
 
     renderData = function(){
@@ -120,18 +125,14 @@ var canvasChart = function(){
 
             var x = 0;
             var y = 0;
-            y = (maxYValue - army) * ratio;
-            if( y < margin.top) army = margin.top;
-            debugger
-             x = (maxXValue - oil) *xRatio;
+            
+            y = (maxYValue - army) * ratio + margin.top ;
+            x = (maxXValue - oil) * xRatio + margin.left;
+            
              population = getPointRadius(set.population)
-
-            var dataPoint = { x: x, y: y, currX: margin.left, x2: prevX, y2: prevY, originalY : set.army, population : population};
+            var dataPoint = { x: x, y: y, population : population};
             finalDataPoints.push(dataPoint);
 
-            prevX = x;
-            prevY = y;
-            drawLines();
             drawPoints();
 
         }
@@ -183,7 +184,7 @@ var canvasChart = function(){
     },
 
     renderCircle = function(x, y, highlightColor, pointRadius = 20){
-        debugger
+        
         x = Math.round(x);
         y = Math.round(y);
         var radgrad = ctx.createRadialGradient(x, y, pointRadius, x - 5, y - 5, 0);
@@ -209,7 +210,6 @@ var canvasChart = function(){
     },
 
     getMaxYValue = function(){
-        debugger
         var maxY = 0;
         for(var year in data_set){
             for (var country in data_set[year]){
@@ -246,7 +246,7 @@ var canvasChart = function(){
         return maxPop;
     },
     getPointRadius = function (pointPopulation){
-        return pointPopulation / maxPop * pointRadius / 10;
+        return pointPopulation / maxPop * pointRadius / 40;
     },
 
     createOverlay = function(){
